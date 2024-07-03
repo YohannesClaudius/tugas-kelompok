d87dc2bf150e206ce0ba125c1cc46eabb5427dcc
johanagustian <johanagustianpohan@gmail.com>
2024-07-03 16:23:12 +0700

    Membuat DatePicker pada halaman home untuk hasil pencarian

--- firstapp/lib/features/controllers/screens/home/widgets/search_result.dart
+++ firstapp/lib/features/controllers/screens/home/widgets/search_result.dart
@@ -95,7 +95,19 @@ class _SearchResultPageState extends State<SearchResultPage> {
     );
   }
 
-  
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: selectedDate ?? DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != selectedDate) {
+      setState(() {
+        selectedDate = picked;
+      });
+    }
+  }  
 
   @override
   Widget build(BuildContext context) {
